6f8831c778e2c512d4beb30148ae1fbfa94d95e5
mounika-nalla <mounika.nalla@ibm.com>
2024-01-09 14:07:21 -0500

    feat: module initial commit SKIP UPGRADE TEST

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,19 @@
-########################################################################################################################
-# Outputs
-########################################################################################################################
+output "instance_id" {
+  description = "The id of the SCC instance created by this module"
+  value       = resource.ibm_resource_instance.scc_instance.id
+}
 
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#}
+output "instance_name" {
+  description = "The name of the SCC instance created by this module"
+  value       = resource.ibm_resource_instance.scc_instance.name
+}
+
+output "location" {
+  description = "The location of the SCC instance created by this module"
+  value       = resource.ibm_resource_instance.scc_instance.location
+}
+
+output "plan" {
+  description = "The plan of the SCC instance created by this module"
+  value       = resource.ibm_resource_instance.scc_instance.plan
+}
--- variables.tf
+++ variables.tf
@@ -1,9 +1,25 @@
-########################################################################################################################
-# Input Variables
-########################################################################################################################
+variable "resource_group_id" {
+  type        = string
+  description = "The id of the resource group to create the SCC instance"
+}
 
-#variable "my_variable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = "default_value"
-#}
+variable "resource_tags" {
+  type        = list(string)
+  description = "A list of tags applied to the resources created by the module"
+  default     = []
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Name of the security and compliance instance that will be provisioned by this module"
+}
+
+variable "plan" {
+  description = "Pricing plan to create SCC instance. Options include Standard or Trial"
+  type        = string
+}
+
+variable "region" {
+  description = "Region where SCC instance will be created"
+  type        = string
+}
