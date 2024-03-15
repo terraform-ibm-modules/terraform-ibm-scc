facfcab0ab8d8f468554c4f048fca865ead7a967
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-03-15 23:00:09 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v7.5.1

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,7 +7,7 @@ module "resource_group" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "7.5.0"
+  version                = "7.5.1"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
