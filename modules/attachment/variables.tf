82b099573288e6c9c7a6d48fc31e0d423cb6c9df
Jordan <jordanwilliams997@hotmail.com>
2024-04-12 14:40:58 +0100

    feat: updated attachment submodule to accept `profile_name` and `profile_version` instead of `profile_id` (#65)

--- modules/attachment/variables.tf
+++ modules/attachment/variables.tf
@@ -1,6 +1,11 @@
-variable "profile_id" {
+variable "profile_name" {
   type        = string
-  description = "ID of the profile you wish to use for the attachment."
+  description = "Name of the SCC profile that is used for the attachment."
+}
+
+variable "profile_version" {
+  type        = string
+  description = "Version of the SCC profile that is used for the attachment."
 }
 
 variable "scc_instance_id" {
