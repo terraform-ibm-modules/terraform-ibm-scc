affa5399f1cb77c3d509e5c2033a8fb578cf3eb7
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-04-15 11:49:54 +0100

    fix: update order

--- moved.tf
+++ moved.tf
@@ -2,3 +2,8 @@ moved {
   from = time_sleep.wait_for_authorization_policy
   to   = time_sleep.wait_for_scc_wp_authorization_policy
 }
+
+moved {
+  from = ibm_scc_provider_type_instance.scc_provider_type_instance_instance
+  to   = ibm_scc_provider_type_instance.scc_provider_type_instance
+}
