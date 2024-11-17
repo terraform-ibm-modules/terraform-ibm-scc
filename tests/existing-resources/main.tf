4c794e44cc6c494e69db8b4e41d6405fde945365
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-11-17 00:10:17 +0000

    fix(deps): update terraform-module (#189)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -7,7 +7,7 @@ module "resource_group" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.14.3"
+  version                = "8.14.5"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
