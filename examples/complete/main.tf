78ecae97be89fd973e7201f775e425759591285e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-12-07 20:33:54 +0000

    chore(deps): update terraform terraform-ibm-modules/event-notifications/ibm to v1.15.4

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -30,7 +30,7 @@ module "cos" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.15.3"
+  version           = "1.15.4"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
