3e4e74b41ac3955141b4e41eb48dc5cf83bf8096
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-01 23:33:57 +0000

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v8.19.5

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 module "cos" {
   count                  = var.existing_scc_instance_crn == null ? 1 : 0
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.19.4"
+  version                = "8.19.5"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -7,7 +7,7 @@ module "resource_group" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.19.4"
+  version                = "8.19.5"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
