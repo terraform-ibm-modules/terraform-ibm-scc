04c7e10fc4fffdae69aad9498e637af4ac97fc92
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-23 15:11:53 +0000

    fix(deps): update terraform-module

--- tests/resources/prereq-resources/main.tf
+++ tests/resources/prereq-resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "8.19.5"
+  version           = "8.21.3"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -29,7 +29,7 @@ module "cos" {
 
 module "scc_wp" {
   source            = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version           = "1.4.3"
+  version           = "1.5.3"
   name              = var.prefix
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
@@ -42,7 +42,7 @@ module "scc_wp" {
 
 module "cloud_monitoring" {
   source                  = "terraform-ibm-modules/observability-instances/ibm//modules/cloud_monitoring"
-  version                 = "3.4.3"
+  version                 = "3.5.0"
   resource_group_id       = module.resource_group.resource_group_id
   region                  = var.region
   instance_name           = "${var.prefix}-mon"
@@ -56,7 +56,7 @@ module "cloud_monitoring" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.18.8"
+  version           = "1.19.4"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
