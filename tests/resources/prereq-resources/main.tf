9245a2f6b03b76916e119ab2379a5b7de154f094
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-27 20:59:57 +0000

    chore(deps): update terraform-module

--- tests/resources/prereq-resources/main.tf
+++ tests/resources/prereq-resources/main.tf
@@ -42,7 +42,7 @@ module "scc_wp" {
 
 module "cloud_monitoring" {
   source                  = "terraform-ibm-modules/observability-instances/ibm//modules/cloud_monitoring"
-  version                 = "3.5.1"
+  version                 = "3.5.2"
   resource_group_id       = module.resource_group.resource_group_id
   region                  = var.region
   instance_name           = "${var.prefix}-mon"
