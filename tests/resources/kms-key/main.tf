38ee59acb60146dc8b74977d1bdf59e701c2710a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-10 09:45:16 +0000

    fix(deps): update terraform-module

--- tests/resources/kms-key/main.tf
+++ tests/resources/kms-key/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "key_protect" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.0.2"
+  version                   = "5.1.2"
   key_protect_instance_name = "${var.prefix}-key-protect"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
